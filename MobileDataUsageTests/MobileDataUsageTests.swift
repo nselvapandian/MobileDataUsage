6ef932818ebfa31e18778698a08f7a6f194b370c
Selva Pandian N <selvapandian.n@zohocorp.com>
2019-01-29 14:41:08 +0530

    UI test case
    Minor changes in uitest cases

--- MobileDataUsageTests/MobileDataUsageTests.swift
+++ MobileDataUsageTests/MobileDataUsageTests.swift
@@ -15,7 +15,17 @@ class MobileDataUsageTests: XCTestCase {
   var allRecords = [Record]()
   
   override func setUp() {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    // Put setup code here. This method is called before the invocation of each test method in the class
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
     self.fetchMobileUSageFromLocal()
     
     self.checkQuarterlyUsageDeviationForTheYear(year: "2005")
@@ -28,15 +38,6 @@ class MobileDataUsageTests: XCTestCase {
     self.getYearlyUsageDataForTheYear(year: "2105")
     
     self.getTotalUsageData()
-  }
-  
-  override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-  }
-  
-  func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
   }
   
   func testPerformanceExample() {
